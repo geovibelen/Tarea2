58080243969387cd648b08fff229d528b8b1cde3
geovibelen <geovibelen2@hotmail.com>
2017-01-20 12:42:27 -0500

    tarea 2

--- app/src/main/java/com/example/chaby/tarea2/ConfirmarDatos.java
+++ app/src/main/java/com/example/chaby/tarea2/ConfirmarDatos.java
@@ -1,13 +1,136 @@
 package com.example.chaby.tarea2;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class ConfirmarDatos extends AppCompatActivity {
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    private TextView txtNombre;
+    private TextView txtFechaNacimiento;
+    private TextView txtTelefono;
+    private TextView txtEmail;
+    private TextView txtDescripcion;
+    private Button btnEditar;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmar_datos);
+
+
+        Bundle parametros = getIntent().getExtras();
+// getString (variable global)
+        String nombre = parametros.getString(getResources().getString(R.string.pnombre));
+        String fecha = parametros.getString(getResources().getString(R.string.pfecha));
+        String telefono = parametros.getString(getResources().getString(R.string.ptelefono));
+        String email = parametros.getString(getResources().getString(R.string.pemail));
+        String descripcion = parametros.getString(getResources().getString(R.string.pdescripcion));
+
+        txtNombre = (TextView) findViewById(R.id.txtNombre);
+        txtFechaNacimiento = (TextView) findViewById(R.id.txtFechaNacimiento);
+        txtTelefono = (TextView) findViewById(R.id.txtTelefono);
+        txtEmail = (TextView) findViewById(R.id.txtEmail);
+        txtDescripcion = (TextView) findViewById(R.id.txtDescripcion);
+
+        txtNombre.setText(nombre);
+        txtFechaNacimiento.setText(fecha);
+        txtTelefono.setText(telefono);
+        txtEmail.setText(email);
+        txtDescripcion.setText(descripcion);
+
+
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("ConfirmarDatos Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+    //Finaliza Actividad
+    public void finalizar(View view) {
+        finish();
+
+
+
+
+
+   }
+
+
+
+    public void confirmar (View v) {
+
+
+        Intent intent = new Intent(ConfirmarDatos.this, MainActivity.class);
+        intent.putExtra(getResources().getString(R.string.pnombre), txtNombre.getText().toString());
+        intent.putExtra(getResources().getString(R.string.pfecha), txtFechaNacimiento.getText().toString());
+        intent.putExtra(getResources().getString(R.string.ptelefono), txtTelefono.getText().toString());
+        intent.putExtra(getResources().getString(R.string.pemail), txtEmail.getText().toString());
+        intent.putExtra(getResources().getString(R.string.pdescripcion), txtDescripcion.getText().toString());
+        startActivity(intent);
+        finish();
+
+
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if (keyCode == android.view.KeyEvent.KEYCODE_BACK) {
+            Intent intent = new Intent(ConfirmarDatos.this, MainActivity.class);
+            startActivity(intent);
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
 }
--- app/src/main/java/com/example/chaby/tarea2/MainActivity.java
+++ app/src/main/java/com/example/chaby/tarea2/MainActivity.java
@@ -1,13 +1,110 @@
 package com.example.chaby.tarea2;
 
+import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
+import android.support.v4.app.DialogFragment;
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.view.View;
+import android.widget.TextView;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TextInputEditText etNombre;
+    private TextInputEditText etFecha;
+    private TextInputEditText etTelefono;
+    private TextInputEditText etEmail;
+    private TextInputEditText etDescripcion;
+    private Button btnSiguiente;
+    private  DatePickerDialog datePickerDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar miActionBar = (Toolbar) findViewById(R.id.miActionBar);
+        setSupportActionBar(miActionBar);
+
+        btnSiguiente = (Button) findViewById(R.id.btnSiguiente);
+
+        etNombre = (TextInputEditText) findViewById(R.id.etNombre);
+        etFecha = (TextInputEditText) findViewById(R.id.etFecha);
+        etTelefono = (TextInputEditText) findViewById(R.id.etTelefono);
+        etEmail = (TextInputEditText) findViewById(R.id.etEmail);
+        etDescripcion = (TextInputEditText) findViewById(R.id.etDescripcion);
+
+        etFecha.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    final Calendar c = Calendar.getInstance();
+                    int mYear = c.get(Calendar.YEAR); // current year
+                    int mMonth = c.get(Calendar.MONTH); // current month
+                    int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+                    // date picker dialog
+                    datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+                            // set day of month , month and year value in the edit text
+                            etFecha.setText(dayOfMonth + "/"
+                                    + (monthOfYear + 1) + "/" + year);
+
+                        }
+                    }, mYear, mMonth, mDay);
+                    datePickerDialog.show();
+                }
+            }
+        });
+
+        try {
+            Bundle parametros = getIntent().getExtras();
+// getString (variable global)
+            String nombre = parametros.getString(getResources().getString(R.string.pnombre));
+            String fecha = parametros.getString(getResources().getString(R.string.pfecha));
+            String telefono = parametros.getString(getResources().getString(R.string.ptelefono));
+            String email = parametros.getString(getResources().getString(R.string.pemail));
+            String descripcion = parametros.getString(getResources().getString(R.string.pdescripcion));
+
+            etNombre.setText(nombre);
+            etFecha.setText(fecha);
+            etTelefono.setText(telefono);
+            etEmail.setText(email);
+            etDescripcion.setText(descripcion);
+        } catch (java.lang.NullPointerException e) {
+
+        }
+
     }
+
+    public void ejecutar (View v) {
+        Intent intent = new Intent(MainActivity.this, ConfirmarDatos.class);
+        intent.putExtra(getResources().getString(R.string.pnombre), etNombre.getText().toString());
+        intent.putExtra(getResources().getString(R.string.pfecha), etFecha.getText().toString());
+        intent.putExtra(getResources().getString(R.string.ptelefono), etTelefono.getText().toString());
+        intent.putExtra(getResources().getString(R.string.pemail), etEmail.getText().toString());
+        intent.putExtra(getResources().getString(R.string.pdescripcion), etDescripcion.getText().toString());
+       startActivity(intent);
+        finish();
+        }
+
+
+
+
+
+
+
+
 }
